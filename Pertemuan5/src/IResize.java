89c69ff24d322723553757fe3f9e43b447a9c0e8
kemal-faza <kemalfaza26@gmail.com>
2026-03-20 14:11:44 +0700

    Menyelesaikan implementasi studi kasusu praktikum 5

---
+++ Pertemuan5/src/IResize.java
@@ -0,0 +1,14 @@
+/**
+ * File : IResize.java
+ * Deskripsi : Class interface untuk melakukan resize
+ * Nama : Muhamad Kemal Faza
+ * Tanggal : 18 Maret 2026
+ */
+
+public interface IResize {
+    public void zoomIn();
+
+    public void zoomOut();
+
+    public void zoom(int percent);
+}
