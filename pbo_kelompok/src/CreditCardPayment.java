9d47ff959fde831cf25bf68ed6aba333bdf883d7
kemal-faza <kemalfaza26@gmail.com>
2026-03-29 21:28:52 +0700

    Menambahkan nama anggota kelompok

--- pbo_kelompok/src/CreditCardPayment.java
+++ pbo_kelompok/src/CreditCardPayment.java
@@ -1,5 +1,12 @@
-package pbo_kelompok.src;
 
+/*
+    Anggota Kelompok:
+    1. Muchammad Yuda Tri Ananda		(24060124110142)
+    2. Muhammad Zaidaan Ardiyansyah		(24060124140200)
+    3. Muhamad Kemal Faza			    (24060124120013)
+    4. Anintya Abhi Wiryateja			(24060124130053)
+    5. Nadia Azura Nurhaniya 			(24060124120019)
+*/
 import java.time.LocalDate;
 
 public class CreditCardPayment implements PaymentMethod {
