8106a4e5247b3c4eab9806ab29bed8961b359d4f
kemal-faza <kemalfaza26@gmail.com>
2026-05-21 13:24:34 +0700

    Menyelesaikan semua implementasi pertemuan 11

---
+++ PBO11A_24060124120013/src/main/java/com/mycompany/pbo11a_24060124120013/PBO11A_24060124120013.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo11a_24060124120013;
+
+/**
+ *
+ * @author kemal
+ */
+public class PBO11A_24060124120013 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
